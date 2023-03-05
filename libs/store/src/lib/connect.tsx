6fbf13e890931209ae9c45211fad2612dbc1a2df
Rick nagual <singloo.yang@gmail.com>
2023-03-05 17:09:17 +0800

    feat: state management lib

---
+++ libs/store/src/lib/connect.tsx
@@ -0,0 +1,72 @@
+import { BaseController } from './types';
+import { unionResolve } from './store';
+import { ClassLike } from '@muse/types';
+import { observer } from 'mobx-react-lite';
+import React, { useEffect } from 'react';
+
+const zip = (arr1: unknown[], arr2: unknown[]): [unknown, unknown][] => {
+  const res: [unknown, unknown][] = [];
+  arr1.forEach((item, idx) => {
+    res.push([item, arr2[idx]]);
+  });
+  return res;
+};
+
+type Enhancer<IncluedProps> = <
+  P extends IncluedProps & JSX.IntrinsicAttributes
+>(
+  Comp: React.FunctionComponent<P>
+) => React.FunctionComponent<
+  Omit<P, keyof IncluedProps> & JSX.IntrinsicAttributes
+>;
+
+function connectStores<
+  Stores extends Record<string, ClassLike<any>>,
+  IncluedProps = { [K in keyof Stores]: any }
+>(stores: Stores): Enhancer<IncluedProps>;
+function connectStores<
+  Stores extends Record<string, ClassLike<any>>,
+  C extends BaseController,
+  IncluedProps = { controller: C } & { [K in keyof Stores]: any }
+>(stores: Stores, controller: ClassLike<C>): Enhancer<IncluedProps>;
+function connectStores<
+  Stores extends Record<string, ClassLike<any>>,
+  C extends BaseController,
+  IncluedProps = { controller?: C } & { [K in keyof Stores]: any }
+>(stores: Stores, controller?: ClassLike<C>): Enhancer<IncluedProps> {
+  const enhanceComp = <P extends IncluedProps & JSX.IntrinsicAttributes>(
+    Comp: React.FunctionComponent<P>
+  ) => {
+    const keys: string[] = (!!controller ? ['controller'] : []).concat(
+      Object.keys(stores)
+    );
+    const classes: ClassLike[] = (!!controller ? [controller] : []).concat(
+      Object.values(stores)
+    );
+    const instances = unionResolve(...classes);
+    const compProps: Partial<
+      { controller?: C } & { [K in keyof Stores]: any }
+    > = Object.fromEntries(zip(keys, instances));
+
+    const CompObserver = observer(Comp);
+    function Enhanced(props: Omit<P, keyof IncluedProps>) {
+      const _props = {
+        ...props,
+        ...compProps,
+      } as P;
+      useEffect(() => {
+        compProps.controller?.componentDidMount();
+        return () => compProps.controller?.componentWillUnmount();
+      }, []);
+      return <CompObserver {..._props} />;
+    }
+    Enhanced.displayName =
+      `connected-with-${controller?.name ?? ''}-${Object.values(stores)
+        .map((o) => o.name)
+        .join('-')}` + Comp.displayName;
+    return Enhanced;
+  };
+  return enhanceComp;
+}
+
+export { connectStores };
---
+++ libs/store/src/lib/store.ts
@@ -0,0 +1,121 @@
+import { Container, injectable } from 'inversify';
+// import 'reflect-metadata';
+import { ClassLike } from '@muse/types';
+
+const CLASS_SYMBOL = Symbol('classSymbol');
+const SINGLETON_SYMBOL = Symbol('isSingleton');
+export const container = new Container();
+
+const USE_SYMBOL_AS_IDENTIFIER = false;
+
+type InjectableConfig = {
+  name?: string;
+  singleton?: boolean;
+};
+export const Injectable = (
+  { name, singleton }: InjectableConfig = {
+    name: undefined,
+    singleton: false,
+  }
+) => {
+  return (target: ClassLike<any>) => {
+    const res = injectable()(target);
+    let classIdentifier = USE_SYMBOL_AS_IDENTIFIER
+      ? Symbol.for(target.name)
+      : target;
+    if (typeof name !== 'undefined' && USE_SYMBOL_AS_IDENTIFIER) {
+      classIdentifier = Symbol(name);
+      Reflect.defineProperty(target, CLASS_SYMBOL, classIdentifier);
+      //   Reflect.defineProperty(CLASS_SYMBOL, classIdentifier, target);
+    }
+    Reflect.defineProperty(target, SINGLETON_SYMBOL, !!singleton);
+    // Reflect.defineMetadata(SINGLETON_SYMBOL, !!singleton, target);
+    const bind = container.bind<typeof target>(classIdentifier).to(target);
+    if (singleton) bind.inSingletonScope();
+
+    return res;
+  };
+};
+
+const getClassSymbol = (target: ClassLike<any>) => {
+  if (!USE_SYMBOL_AS_IDENTIFIER) return target;
+  const storedSymbol = Reflect.get(target, CLASS_SYMBOL);
+  //   const storedSymbol = Reflect.getMetadata(CLASS_SYMBOL, target);
+  return storedSymbol ?? Symbol.for(target.name);
+};
+
+export const checkIfContainsCircleDeps = (
+  chain: ClassLike<any>[],
+  nowResolving: ClassLike<any>
+) => {
+  const hasBeenRequired = chain.some((c) => c === nowResolving);
+  return hasBeenRequired;
+};
+
+const isClass = (target: new (...args: unknown[]) => unknown) => {
+  return (
+    typeof target === 'function' &&
+    /^class\s/.test(target.prototype.constructor.toString())
+  );
+};
+export const resolve = <T>(
+  target: ClassLike<T>,
+  sharedContaienr?: Container
+): T => {
+  if (!isClass(target)) throw new Error('Invalid target, expect Class');
+  const identifier = getClassSymbol(target); // identifier of class
+  const paramTypes = Reflect.get(target, 'design:paramtypes');
+  //   const paramTypes = Reflect.getMetadata('design:paramtypes', target);
+  if (!paramTypes?.length) {
+    // no parameters, create an instance
+    const isBound = container.isBound(identifier);
+    if (isBound) {
+      const isSingleton = Reflect.get(target, SINGLETON_SYMBOL);
+      //   const isSingleton = Reflect.getMetadata(SINGLETON_SYMBOL, target);
+      const instance = container.get(identifier) as T;
+      // if target is a singleton or no shared container exists, return instance
+      if (isSingleton || !sharedContaienr) return instance;
+      // for non-singleton class, register instance, return instance from shared container
+      if (!sharedContaienr.isBound(identifier)) {
+        sharedContaienr.bind(identifier).toConstantValue(instance);
+      }
+      return sharedContaienr.get(identifier);
+    }
+    return new target();
+  }
+
+  const args: unknown[] = (paramTypes as any[]).map((param) =>
+    resolve(param, sharedContaienr)
+  );
+  return new target(...args);
+};
+
+function unionResolve<T1>(...args: [ClassLike<T1>]): [T1];
+function unionResolve<T1, T2>(
+  ...args: [ClassLike<T1>, ClassLike<T2>]
+): [T1, T2];
+function unionResolve<T1, T2, T3>(
+  ...args: [ClassLike<T1>, ClassLike<T2>, ClassLike<T3>]
+): [T1, T2, T3];
+function unionResolve<T1, T2, T3, T4>(
+  ...args: [ClassLike<T1>, ClassLike<T2>, ClassLike<T3>, ClassLike<T4>]
+): [T1, T2, T3, T4];
+function unionResolve<T1, T2, T3, T4, T5>(
+  ...args: [
+    ClassLike<T1>,
+    ClassLike<T2>,
+    ClassLike<T3>,
+    ClassLike<T4>,
+    ClassLike<T5>
+  ]
+): [T1, T2, T3, T4, T5];
+function unionResolve(...targets: ClassLike<any>[]): any[]; // TODO, resolve type hints
+function unionResolve(...targets: ClassLike<any>[]): any[] {
+  const sharedContainer = new Container();
+  return targets.map((target) => resolve(target, sharedContainer));
+}
+
+export { unionResolve };
+export const Instantiable = () => {
+  return (target: ClassLike<any>) => void 0;
+};
