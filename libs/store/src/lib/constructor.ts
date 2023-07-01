92a8c0204560f5a4934dd06d2cfda6542fd2e7d2
Rick nagual <singloo.yang@gmail.com>
2023-07-01 13:12:56 +0800

    wip: construct store

--- libs/store/src/lib/constructor.ts
+++ libs/store/src/lib/constructor.ts
@@ -3,18 +3,21 @@ import { createContext } from 'react';
 import { createInjectable, createInstantiable } from './decorators';
 import { createResolver, createUnionResolver } from './resolver';
 import { StoreContextType } from './types';
+import { createUseSingletonHook, createUseUnionResolveHook } from './hooks';
 
 export const constructStore = (container: Container) => {
   const Injectable = createInjectable(container);
   const Instantiable = createInstantiable();
   const resolver = createResolver(container);
   const unionResolver = createUnionResolver(resolver);
-
   const context: StoreContextType = createContext({
     container,
     resolver,
     unionResolver,
   });
+
+  const useInstance = createUseSingletonHook(context);
+  const useUnionResolve = createUseUnionResolveHook(context);
 
   return {
     Injectable,
@@ -22,5 +25,7 @@ export const constructStore = (container: Container) => {
     resolver,
     unionResolver,
     context,
+    useInstance,
+    useUnionResolve,
   };
 };
