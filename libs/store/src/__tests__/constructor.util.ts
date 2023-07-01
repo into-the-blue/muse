a64181c1b66e5efeafc558ad77d87616b5e6dc83
Rick nagual <singloo.yang@gmail.com>
2023-07-01 13:48:07 +0800

    test: resolve hook

---
+++ libs/store/src/__tests__/constructor.util.ts
@@ -0,0 +1,82 @@
+import { makeObservable, observable, action } from 'mobx';
+import { Container } from 'inversify';
+import { constructStore } from '../lib/constructor';
+
+const container = new Container();
+
+const { Injectable, Instantiable, useInstance, useUnionResolve } =
+  constructStore(container);
+
+export { useInstance, useUnionResolve };
+
+@Injectable({ singleton: true })
+export class Store1 {
+  constructor() {
+    makeObservable(this, {
+      count: observable,
+      increase: action,
+    });
+  }
+  count = 0;
+  increase = () => {
+    this.count += 1;
+  };
+}
+
+@Injectable()
+export class Store2 {
+  constructor() {
+    makeObservable(this, {
+      count2: observable,
+      increase2: action,
+    });
+  }
+  count2 = 0;
+
+  increase2 = () => {
+    this.count2 += 2;
+  };
+}
+
+@Injectable()
+export class Service1 {
+  store1: Store1;
+  constructor(store1: Store1) {
+    this.store1 = store1;
+  }
+
+  invoke = () => {
+    this.store1.increase();
+  };
+  get getCount() {
+    return this.store1.count;
+  }
+}
+
+@Injectable()
+export class Service2 {
+  constructor(private store2: Store2) {}
+  invoke = () => {
+    this.store2.increase2();
+  };
+  get getCount() {
+    return this.store2.count2;
+  }
+}
+
+@Instantiable()
+export class Controller1 {
+  constructor(private service1: Service1, private service2: Service2) {}
+  invoke1 = () => {
+    this.service1.invoke();
+  };
+  invoke2 = () => {
+    this.service2.invoke();
+  };
+  get service1Count() {
+    return this.service1.getCount;
+  }
+  get service2Count() {
+    return this.service2.getCount;
+  }
+}
