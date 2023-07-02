c2bf4ff0cd4a635c50a3fad960663a4e532f6ae9
Rick nagual <singloo.yang@gmail.com>
2023-07-02 19:50:54 +0800

    fix: version

--- syncpack.config.js
+++ syncpack.config.js
@@ -7,13 +7,31 @@ const config = {
     'packages/*/package.json',
     'apps/*/package.json',
     'libs/*/package.json',
+    'package.json',
+  ],
+  semverGroups: [
+    {
+      range: '~',
+      dependencies: ['**'],
+      packages: ['muse'],
+    },
+    {
+      packages: ['@muse/*'],
+      isIgnored: true,
+      dependencies: ['**'],
+    },
   ],
   versionGroups: [
-    // {
-    //   packages: ['**'],
-    //   dependencies: ['**'],
-    //   snapTo: ['muse'],
-    // },
+    {
+      packages: ['@muse/note-spider'],
+      dependencies: ['**'],
+      isIgnored: true,
+    },
+    {
+      packages: ['@muse/*', 'muse'],
+      dependencies: ['**'],
+      snapTo: ['muse'],
+    },
   ],
 };
 
