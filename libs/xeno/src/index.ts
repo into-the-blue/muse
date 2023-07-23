616bb4022dd76dbae54a38b9add3285bff244b4f
Rick nagual <singloo.yang@gmail.com>
2023-07-23 19:03:02 +0800

    fix type

---
+++ libs/xeno/src/index.ts
@@ -0,0 +1,6 @@
+export { Xeno } from './lib/xeno';
+export type { IXenoInjectedProps, TXenoMessage } from './lib/type';
+
+export { connectXeno } from './lib/connect';
+
+export { constrcutXeno } from './lib/construtor';
