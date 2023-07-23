3ac314a6a4e2f1c41dca330cac7756f324e993e5
Rick nagual <singloo.yang@gmail.com>
2023-07-23 20:27:34 +0800

    wip: add xeno

---
+++ apps/muse-mobile/src/integrations/xeno.ts
@@ -0,0 +1,12 @@
+import { constrcutXeno } from '@muse/xeno';
+
+const { xeno, useXeno, useXenoListener, useXenoTrigger, Enhancer } =
+  constrcutXeno();
+
+export {
+  xeno,
+  useXeno,
+  useXenoListener,
+  useXenoTrigger,
+  Enhancer as xenoEnhancer,
+};
--- libs/types/src/lib/common.ts
+++ libs/types/src/lib/common.ts
@@ -1,1 +1,4 @@
 export type ClassLike<T = any> = new (...args: any[]) => T;
+export type Enhancer = <P extends JSX.IntrinsicAttributes>(
+  app: React.ComponentType<P>
+) => React.ComponentType<P>;
