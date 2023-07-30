5ef68511a25aa884b78fccc23c4fc60134a50fd3
Rick nagual <singloo.yang@gmail.com>
2023-07-30 19:17:38 +0800

    chore

--- apps/muse-mobile/src/integrations/navigation.tsx
+++ apps/muse-mobile/src/integrations/navigation.tsx
@@ -1,6 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Home } from '../pages/Home';
+import { Home } from '../pages/Home1';
 
 const Stack = createNativeStackNavigator();
 
