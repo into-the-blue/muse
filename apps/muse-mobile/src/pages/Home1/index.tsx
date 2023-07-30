a54dbd0caaaec24864987c1f8be082993b814f34
Rick nagual <singloo.yang@gmail.com>
2023-07-30 19:17:38 +0800

    chore

--- apps/muse-mobile/src/pages/Home1/index.tsx
+++ apps/muse-mobile/src/pages/Home1/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet, StatusBar } from 'react-native';
+import { StartReview } from './StartReview';
 
 export const Home = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
+        <StartReview />
       </SafeAreaView>
     </>
   );
