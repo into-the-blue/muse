d9196759abb4a9ffb59ce9bdc96be48ec01ee2e5
Rick nagual <singloo.yang@gmail.com>
2023-07-30 14:56:31 +0800

    fix type

--- libs/xeno/src/index.ts
+++ libs/xeno/src/index.ts
@@ -1,6 +1,6 @@
 export { Xeno } from './lib/xeno';
-export type { IXenoInjectedProps, TXenoMessage } from './lib/type';
-
+export type { IXenoInjectedProps, TXenoMessage, XenoEmitter } from './lib/type';
+export type { Observable } from 'rxjs';
 export { connectXeno } from './lib/connect';
 
 export { constrcutXeno } from './lib/construtor';
