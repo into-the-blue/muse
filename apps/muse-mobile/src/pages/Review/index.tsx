98fb2636f069d807bfc0f8d92572eb92333b2e82
Rick nagual <singloo.yang@gmail.com>
2023-07-30 19:16:10 +0800

    chore

---
+++ apps/muse-mobile/src/pages/Review/index.tsx
@@ -0,0 +1,5 @@
+import { View } from 'react-native';
+
+export const Review = () => {
+  return <View>{'review page'}</View>;
+};
---
+++ apps/muse-mobile/src/pages/home/StartReview/index.tsx
@@ -0,0 +1,11 @@
+import { View, Button } from 'react-native';
+import { Text } from '@muse/ui-native';
+
+export const StartReview = () => {
+  return (
+    <View>
+      <Text>{'start review'}</Text>
+      <Button title={'increase'} />
+    </View>
+  );
+};
