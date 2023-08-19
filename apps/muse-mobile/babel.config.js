3197be0c3fc5385a3ed8439f7534e50860ae3228
Rick nagual <singloo.yang@gmail.com>
2023-08-19 17:46:41 +0800

    fix: babel

--- apps/muse-mobile/babel.config.js
+++ apps/muse-mobile/babel.config.js
@@ -4,7 +4,7 @@ module.exports = function (api) {
     plugins: [
       'babel-plugin-transform-typescript-metadata',
       ['@babel/plugin-proposal-decorators', { legacy: true }],
-      ['@babel/plugin-proposal-class-properties', { loose: true }],
+      // ['@babel/plugin-proposal-class-properties', { loose: true }], // @TODO this plugin will disable mixins
     ],
     presets: ['babel-preset-expo'],
   };
