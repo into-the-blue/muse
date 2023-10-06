9aafbd70d099913a8e6dc32d516242246bc7860d
Rick nagual <singloo.yang@gmail.com>
2023-10-06 14:56:14 +0800

    fix: hmr reset store

--- libs/store/src/lib/hooks.ts
+++ libs/store/src/lib/hooks.ts
@@ -5,8 +5,11 @@ import type { ClassLike } from '@muse/types';
 export const createUseSingletonHook = (context: StoreContextType) => {
   const useSingleton = <T>(storeClass: ClassLike<T>) => {
     const { resolver } = useContext(context);
+    const _result = useRef<T>();
     const result = useMemo(() => {
-      return resolver(storeClass);
+      if (_result.current) return _result.current;
+      _result.current = resolver(storeClass);
+      return _result.current;
     }, [storeClass, resolver]);
     return result;
   };
@@ -18,8 +21,11 @@ export const createUseUnionResolveHook = (context: StoreContextType) => {
   const useUnionResolve: UnionResolver = (...classes: any[]) => {
     const { unionResolver } = useContext(context);
     const classRef = useRef(classes);
+    const _results = useRef<any[]>();
     const results = useMemo(() => {
-      return unionResolver(...classRef.current);
+      if (_results.current) return _results.current;
+      _results.current = unionResolver(...classRef.current);
+      return _results.current;
     }, [unionResolver]);
     return results;
   };
