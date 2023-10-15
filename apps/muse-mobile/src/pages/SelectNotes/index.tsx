c93295edd22beb1c47faf83fb45681f242e13e6f
Rick nagual <singloo.yang@gmail.com>
2023-10-15 16:28:49 +0800

    chore: test reanimated

--- apps/muse-mobile/src/pages/SelectNotes/index.tsx
+++ apps/muse-mobile/src/pages/SelectNotes/index.tsx
@@ -1,10 +1,23 @@
 import { Text } from '@muse/ui-native';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, Button } from 'react-native';
+import Animated, { useSharedValue, withSpring } from 'react-native-reanimated';
 
 export const SelectNotes = () => {
+  const width = useSharedValue(100);
+  const onPress = () => {
+    width.value = withSpring(width.value + 50);
+  };
   return (
     <SafeAreaView>
       <Text>{'Select notes'}</Text>
+      <Animated.View
+        style={{
+          width,
+          height: 100,
+          backgroundColor: 'violet',
+        }}
+      />
+      <Button onPress={onPress} title={'press me'} />
     </SafeAreaView>
   );
 };
