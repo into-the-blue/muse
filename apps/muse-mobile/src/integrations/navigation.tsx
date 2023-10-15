ddc130eaa6a859560ce553c5f037e0ce41ee003b
Rick nagual <singloo.yang@gmail.com>
2023-10-15 15:56:09 +0800

    feat: navigation type

--- apps/muse-mobile/src/integrations/navigation.tsx
+++ apps/muse-mobile/src/integrations/navigation.tsx
@@ -1,16 +1,17 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Home } from '../pages/Home';
+import { Lobby } from '../pages/Lobby';
 import { SelectNotes } from '../pages/SelectNotes';
 import { Review } from '../pages/Review';
+import type { NavigationRouterList } from '@muse/types';
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<NavigationRouterList>();
 
 export const Navigation = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{}} initialRouteName={'Home'}>
-        <Stack.Screen name={'Home'} component={Home} />
+        <Stack.Screen name={'Home'} component={Lobby} />
         <Stack.Screen name={'SelectNotes'} component={SelectNotes} />
         <Stack.Screen name={'Review'} component={Review} />
       </Stack.Navigator>
--- libs/types/src/mobile/routeName.ts
+++
@@ -1,3 +0,0 @@
-export enum RouteNames {
-  Home = 'Home',
-}
